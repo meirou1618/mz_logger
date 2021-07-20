d274c2433131f1cedf90510ecdd380295f51d7ca
@sato.m <sato.mizuki.mllab.nit@gmail.com>
2021-07-20 11:04:52 +0900

    0720 setpu file

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+form setuptools import find_packages, setup
+
+setup(
+    name='logger',
+    version='0.1.0',
+    description='mz logger',
+    packages=find_packages(),
+)
